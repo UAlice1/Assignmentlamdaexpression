a85c97a6e0ae0a8ed1344995b1dd73e936831c25
UAlice1 <umubyeyialice7@gmail.com>
2025-05-04 01:21:11 +0100

    Initial commit with README.md

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        List<Integer> mixed = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> evens1 = EvenNumberFilter.filterEvenNumbers(mixed);
+        System.out.println("Original List: " + mixed);
+        System.out.println("Even Numbers List: " + evens1);
+        System.out.println();
+
+        List<Integer> empty = Collections.emptyList();
+        List<Integer> evens2 = EvenNumberFilter.filterEvenNumbers(empty);
+        System.out.println("Original List: " + empty);
+        System.out.println("Even Numbers List: " + evens2);
+        System.out.println();
+
+        List<Integer> oddOnly = Arrays.asList(1, 3, 5, 7, 9);
+        List<Integer> evens3 = EvenNumberFilter.filterEvenNumbers(oddOnly);
+        System.out.println("Original List: " + oddOnly);
+        System.out.println("Even Numbers List: " + evens3);
     }
 }
